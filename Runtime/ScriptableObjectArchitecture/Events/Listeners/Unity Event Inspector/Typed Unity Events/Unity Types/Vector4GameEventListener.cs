169f4cca7e067e5966fbbd71e8ead990dcede9e8
James LaFritz <ktmarine1999+git@gmail.com>
2023-11-20 12:46:12 -0500

    doc(Manual, API): Modify Unity Types for documentation
    This commit is part of a rebase to fix commit messages to new standards and cherry-picking commit c5de31c.
     It modifies Unity Types for documentation purposes.
    
    Date: Mon Nov 20 12:46:12 2023 -0500
    
    Changes:
    - Modified Vector4GameEventListener.cs in Unity Event Inspector/Typed Unity Events/Unity Types
    - Added ExposedReferenceAttribute documentation
    - Modified various documentation files related to logging, attributes, and properties

--- Runtime/ScriptableObjectArchitecture/Events/Listeners/Unity Event Inspector/Typed Unity Events/Unity Types/Vector4GameEventListener.cs
+++ Runtime/ScriptableObjectArchitecture/Events/Listeners/Unity Event Inspector/Typed Unity Events/Unity Types/Vector4GameEventListener.cs
@@ -2,13 +2,10 @@
 
 namespace CoreFramework.ScriptableObjectArchitecture.Events
 {
-    namespace CoreFramework.ScriptableObjectArchitecture.UnityEventListeners
-    {
-        /// <summary>
-        /// The vector4 game event listener class
-        /// </summary>
-        /// <seealso cref="UnityGameEventListener{Vector4, Vector4GameEvent, Vector4UnityEvent}"/>
-        [AddComponentMenu(Utility.EventListenerSubmenu + "Vector4 Event Listener")]
-        public sealed class Vector4GameEventListener : UnityGameEventListener<Vector4, Vector4GameEvent, Vector4UnityEvent> { }
-    }
+    /// <summary>
+    /// The vector4 game event listener class
+    /// </summary>
+    /// <seealso cref="UnityGameEventListener{Vector4, Vector4GameEvent, Vector4UnityEvent}"/>
+    [AddComponentMenu(Utility.EventListenerSubmenu + "Vector4 Event Listener")]
+    public sealed class Vector4GameEventListener : UnityGameEventListener<Vector4, Vector4GameEvent, Vector4UnityEvent> { }
 }

adfa8fbf7dd7c1f4c3f0ac4ed9bbe99f22d9fe52
James LaFritz <ktmarine1999+git@gmail.com>
2024-02-07 00:27:50 -0500

    feat: Add ExposedReferenceAttribute
    This commit adds the ExposedReferenceAttribute, which can be applied to fields or properties to make reference types appear as foldout properties in the Inspector.
    This is particularly useful for ScriptableObjects.

---
+++ Runtime/Attributes/Properties/Modifiers/ExposedReferenceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace CoreFramework.Attributes
+{
+    /// <summary>
+    /// Use this attribute to make a reference Property Field type appear as a foldout property i.e. ScriptableObjects in the <i><b>Inspector</b></i>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class ExposedReferenceAttribute : PropertyAttribute { }
+}

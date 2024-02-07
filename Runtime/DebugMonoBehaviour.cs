c9d8f1f52762df62b3d486da5c961f6a27a6090d
James LaFritz <ktmarine1999+git@gmail.com>
2024-02-07 00:01:12 -0500

    feat(Runtime): Add DebugMonoBehaviour for logging debug information
    This commit introduces a new MonoBehaviour class, `DebugMonoBehaviour`, designed to facilitate logging of debug information within MonoBehaviour scripts.
    It provides methods for logging informational, error, and warning messages, along with the method and behaviour names.
    The class includes an option to toggle the display of debug information.

---
+++ Runtime/DebugMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace CoreFramework
+{
+    public class DebugMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField] private bool showDebugInfo = true;
+        
+        protected void LogInfo(string message, string callingMethod, string callingBehaviour)
+        {
+            if (!showDebugInfo) return;
+            Debug.Log($"<size=18><b><color=blue>{name}: <i>{callingBehaviour}</i></color></b>" +
+                      $"-<i><color=brown>{callingMethod}</color></i>" +
+                      $"- {message}</size>", gameObject);
+        }
+        
+        protected void LogError(string message, string callingMethod, string callingBehaviour)
+        {
+            Debug.LogError($"<size=25><b><color=maroon>{name}: <i>{callingBehaviour}</i></color></b>" +
+                           $"-<i><color=purple>{callingMethod}</color></i>" +
+                           $"- <color=red>{message}</color></size>", gameObject);
+        }
+        
+        protected void LogWarning(string message, string callingMethod, string callingBehaviour)
+        {
+            if (!showDebugInfo) return;
+            
+            Debug.LogWarning($"<size=20><b><color=orange>{name}: <i>{callingBehaviour}</i></color></b>" +
+                             $"-<i><color=olive>{callingMethod}</color></i>" +
+                             $"- <color=yellow>{message}</color></size>", gameObject);
+        }
+    }
+}
